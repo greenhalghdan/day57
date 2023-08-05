e0b9a54f42a9c5343f87c91e219bc37474a576fb
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2023-08-06 00:43:10 +0100

    blog

--- server.py
+++ server.py
@@ -28,6 +28,12 @@ def guess(guess_name):
     age = ageresponse.json()
     return render_template("guess.html", name=name.title(), GENDER=gender["gender"], AGE=age["age"])
 
+@app.route("/blog")
+def blod():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
