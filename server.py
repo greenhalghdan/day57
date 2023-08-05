7b7282f22a99d872e46547846d6754b960ff0ab7
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2023-08-05 23:48:10 +0100

    templating

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
-
+import random
+import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World"
+    random_number = random.randint(1, 10)
+    year = datetime.datetime.now()
+    name = "Daniel Greenhalgh"
+    return render_template("index.html", num=random_number, NAME=name, CURRENT_YEAR=year.year)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
